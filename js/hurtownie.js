f4ec17f5dfe912964a4bdb28bd6d8c90876e782a
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2018-10-22 12:01:41 +0200

    Update wyszukiwarki
    Dziala powierchnia i typu sprzedazy i typy domow

--- js/hurtownie.js
+++ js/hurtownie.js
@@ -19,7 +19,7 @@ hurtownie.controller("mainCtrl", function($scope, $http) {
 		
 		let wskaznikStrony = 1;
 		//Jest to ilosc stron niezbedna do otworzenia, aby pobrac ilosc rekordow podana przez uzytkownika
-		let iloscStron = Math.ceil(c.maxIloscRekordow / 26);
+		let iloscStron = Math.ceil(c.maxIloscRekordow / 27);
 		
 		pobierzOgloszeniaRec();
 		
@@ -43,9 +43,9 @@ hurtownie.controller("mainCtrl", function($scope, $http) {
 					let ogloszenieJquery = $(this);
 					let ogloszenie = {};
 					ogloszenie["nazwa ogloszenia"] = ogloszenieJquery.find(".offer-item-title").text();
-					ogloszenie["cena"] = ogloszenieJquery.find(".offer-item-rooms").text();
-					ogloszenie["liczba pokoi"] = ogloszenieJquery.find(".offer-item-area").text();
-					ogloszenie["metraz"] = ogloszenieJquery.find(".offer-item-price").text();
+					ogloszenie["cena"] = ogloszenieJquery.find(".offer-item-price").text();
+					ogloszenie["liczba pokoi"] = ogloszenieJquery.find(".offer-item-rooms").text();
+					ogloszenie["metraz"] = ogloszenieJquery.find(".offer-item-area").text();
 					c.ogloszenia.push(ogloszenie);
 				});
 			},
@@ -54,15 +54,33 @@ hurtownie.controller("mainCtrl", function($scope, $http) {
 			}
 		);
 	}
-	
-	function createUrl() {
+// Pierwotna wersja funkcji createUrl ktora na pewno dziala	
+//	function createUrl() {
+//		let url='';
+//		url = 'https://www.otodom.pl/sprzedaz/dom/'
+//					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
+//					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
+//					+ '&search%5Bdescription%5D=1'
+//		return url;
+//	}
+    function createUrl() {
 		let url='';
-		url = 'https://www.otodom.pl/sprzedaz/dom/'
+        if (c.param.powMin !== null && c.param.powMax !== null){
+		url = 'https://www.otodom.pl/'+ c.param.typsprzedazy + '/' + c.param.typdomu+'/'
 					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
 					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
-					+ '&search%5Bdescription%5D=1'
+                    + '&search%5Bfilter_float_m%3Afrom%5D=' + c.param.powMin
+                    + '&search%5Bfilter_float_m%3Ato%5D=' + c.param.powMax
+					+ '&search%5Bdescription%5D=1' 
+        }else{
+            url = 'https://www.otodom.pl/'+ c.param.typsprzedazy + '/' + c.param.typdomu+'/'
+					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
+					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
+					+ '&search%5Bdescription%5D=1' 
+        }
 		return url;
 	}
+    
 });
 
 
