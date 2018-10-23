2bef71b2ee081f8406d8c4e8cc42463ca433856e
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2018-10-23 16:34:53 +0200

    Działa kurła

---
+++ hurtownieDB.sql
@@ -0,0 +1,7 @@
+DROP DATABASE hurtownieDB;
+CREATE DATABASE hurtownieDB;
+CREATE TABLE IF NOT EXISTS userDatabase(ID INT PRIMARY KEY ,cenaMin INT, cenaMax INT, powMin INT, powMax INT, woj TEXT, typdomu TEXT, typsprzedazy TEXT);
+USE hurtownieDB;
+INSERT INTO userdatabase(ID,cenaMin,cenaMax,powMin,powMax,woj,typdomu,typsprzedazy) VALUES(1, 50, 5000, 10, 100, "slaskie", "dom", "sprzedaz");
+INSERT INTO userdatabase(ID,cenaMin,cenaMax,powMin,powMax,woj,typdomu,typsprzedazy) VALUES(2, 502, 50002, 102, 1002, "malopolskie", "mieszkanie", "wynajem");
+DELETE FROM userDatabase WHERE ID = 2;
