29e3c56a49a4cbabf04748e006a4113a6aa0054a
gege3pp5 <33030360+gege3pp5@users.noreply.github.com>
2018-12-15 18:24:05 +0100

    refactoring + drobne poprawki

--- js/hurtownie.js
+++ js/hurtownie.js
@@ -1,32 +1,121 @@
+String.prototype.poAngielskiemu = function() {
+	let str = this.toString();
+	return str.replace(/[ąĄćĆęĘłŁńŃóÓśŚźŹżŻ]/g, (ch) => {
+		let lowerCaseChar = ch.toLowerCase();			
+		let normalizedLowerChar = (function() {
+			switch(lowerCaseChar) {
+				case 'ą': return 'a'
+				case 'ć': return 'c'
+				case 'ę': return 'e'
+				case 'ł': return 'l'
+				case 'ń': return 'n'
+				case 'ó': return 'o'
+				case 'ś': return 's'
+				case 'ź': return 'z'
+				case 'ż': return 'z'
+			}
+		})();
+		let shouldCapitalize = ch != lowerCaseChar; 
+		return shouldCapitalize ? normalizedLowerChar.toUpperCase() : normalizedLowerChar;
+	})
+}
+
+
 let hurtownie = angular.module('hurtownie', []);
 
-// Zmienna container oraz licznik i wykorzystywane sa przy tworzeniu i przechowywaniu zmiennych pobranych ze strony
-var container = {};
-let i = 1;   
-// Utworzenie klasy ObjectInJS umozliwia przeslanie plikow do JSON;
-    function ObjectInJS(nazwa,cena,lpok,met,id){
-        this.nazwa = nazwa;
-        this.cena = cena;
-        this.lpok = lpok;
-        this.met = met;
-        this.id = id;
-    }
-
-
 hurtownie.controller("mainCtrl", function($scope, $http) {
-	$scope.x = 5;
-	let c = this;
-	c.param = {
-        id: null,
-		cenaMin: null,
-		cenaMax: null,
-		powMin: null,
-		powMax: null,
-		woj: null,
-		typdomu: null,
-		typsprzedazy: null
-	}
+	let mainC = this;
+	
+	mainC.regions = [
+		'WSZYSTKIE',
+		'dolnośląskie',
+		'kujawsko-pomorskie',
+		'lubelskie',
+		'lubuskie',
+		'łódzkie',
+		'małopolskie',
+		'mazowieckie',
+		'opolskie',
+		'podkarpackie',
+		'podlaskie',
+		'pomorskie',
+		'śląskie',
+		'świętokrzyskie',
+		'warmińsko-mazurskie',
+		'wielkopolskie',
+		'zachodniopomorskie',
+	];
 
+	mainC.activePage = 'etl';
+	
+	mainC.navigate = function(pageName) {
+		mainC.activePage = pageName;
+	}
+});
+
+hurtownie.controller("homeCtrl", function($scope, $http) {
+	let c = this;
+});
+
+hurtownie.controller("etlCtrl", function($scope, $http) {
+	let c = this;
+	
+	c.nextStep = 'e';
+	
+	c.searchParams = {
+		buildingType: 'dom',
+		contractType: 'wynajem',
+		priceMin: null,
+		priceMax: null,
+		areaMin: null,
+		areaMax: null,
+		region: 'WSZYSTKIE',
+	};
+		
+	c.performEtl = function() {
+		let baseUrl = buildBaseUrl(c.searchParams);
+		console.log(baseUrl);
+	}
+	
+	c.extract = function() {
+		c.nextStep = 't';
+	}
+	
+	c.transform = function() {
+		c.nextStep = 'l'
+	}
+	
+	c.load = function() {
+		c.nextStep = 'e'
+	}
+	
+	function buildBaseUrl(searchParams) {
+		let url = 'https://www.otodom.pl'
+			+ '/' + c.searchParams.contractType.poAngielskiemu()
+			+ '/' + c.searchParams.buildingType.poAngielskiemu()
+			+ ((c.searchParams.region === 'WSZYSTKIE')
+				? ''
+				: ('/' + c.searchParams.region.poAngielskiemu()))
+			+ '?';
+		let urlParams = [];
+		if(c.searchParams.priceMin !== null)
+			urlParams.push('search%5Bfilter_float_price%3Afrom%5D=' + c.searchParams.priceMin);
+		if(c.searchParams.priceMax !== null)
+			urlParams.push('search%5Bfilter_float_price%3Ato%5D=' + c.searchParams.priceMax);
+		if(c.searchParams.areaMin !== null)
+			urlParams.push('search%5Bfilter_float_m%3Afrom%5D=' + c.searchParams.areaMin);
+		if(c.searchParams.areaMax !== null)
+			urlParams.push('search%5Bfilter_float_m%3Ato%5D=' + c.searchParams.areaMax);
+		url += urlParams.join('&') + '&search%5Bdescription%5D=1';
+		return url;
+	}
+});
+
+hurtownie.controller("dbCtrl", function($scope, $http) {
+	let c = this;
+});
+
+/*
 
 	c.ogloszenia = [];
 	c.maxIloscRekordow = 50;
@@ -86,35 +175,8 @@ hurtownie.controller("mainCtrl", function($scope, $http) {
 			}
 		);
 	}
-
-// Pierwotna wersja funkcji createUrl ktora na pewno dziala	
-//	function createUrl() {
-//		let url='';
-//		url = 'https://www.otodom.pl/sprzedaz/dom/'
-//					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
-//					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
-//					+ '&search%5Bdescription%5D=1'
-//		return url;
-//	}
-    function createUrl() {
-		let url='';
-        if (c.param.powMin !== null && c.param.powMax !== null){
-		url = 'https://www.otodom.pl/'+ c.param.typsprzedazy + '/' + c.param.typdomu+'/'
-					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
-					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
-                    + '&search%5Bfilter_float_m%3Afrom%5D=' + c.param.powMin
-                    + '&search%5Bfilter_float_m%3Ato%5D=' + c.param.powMax
-					+ '&search%5Bdescription%5D=1' 
-        }else{
-            url = 'https://www.otodom.pl/'+ c.param.typsprzedazy + '/' + c.param.typdomu+'/'
-					+ '?search%5Bfilter_float_price%3Afrom%5D=' + c.param.cenaMin
-					+ '&search%5Bfilter_float_price%3Ato%5D=' + c.param.cenaMax
-					+ '&search%5Bdescription%5D=1' 
-        }
-		return url;
-	}
     
-});
+});*/
 
 
 
